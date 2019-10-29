2e3d317275eae3b4f926030a8e5349792cf14811
Greg <gntsketches@gmail.com>
2019-10-28 19:42:45 -0700

    got code ready for rh/lh switch

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 const fullRange = ["C1","C#1","D1","D#1","E1","F1","F#1","G1","G#1","A1","A#1","B1","C2","C#2","D2","D#2","E2","F2","F#2","G2","G#2","A2","A#2","B2","C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3","C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4","C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5","C6","C#6","D6","D#6","E6","F6","F#6","G6","G#6","A6","A#6","B6","C7","C#7","D7","D#7","E7","F7","F#7","G7","G#7","A7","A#7","B7","C8", "C#8","D8","D#8","E8","F8","F#8","G8","G#8","A8","A#8","B8"]
 const intervalConversions = {
   'cur': 0,
-  '+m1': 1,
-  '-m1': -1,
+  '+m2': 1,
+  '-m2': -1,
   '+M2': 2,
   '-M2': -2,
   '+m3': 3,
@@ -19,17 +19,38 @@ const intervalConversions = {
   '+8v': 12,
   '-8v': -12,
 }
+
+const LH_pitch_keys = {
+  'space': 'cur',
+  'f': '+m2', 'd': '-m2', 'g': '+M2', 's': '-M2',
+  'r': '+m3', 'e': '-m3', 't': '+M3', 'w': '-M3',
+  '4': '+P4', '3': '-P4', '5': '+tt', '2': '-tt',
+  'c': '+P5', 'x': '-P5', 'v': '+8v', 'z': '-8v',
+}
+
+const RH_pitch_keys = {
+  'k': '+m2', 'j': '-m2', 'h': '+M2', 'l': '-M2',
+  'i': '+m3', 'u': '-m3', 'o': '+M3', 'y': '-M3',
+  '9': '+P4', '8': '-P4', '0': '+tt', '7': '-tt',
+  'm': '+P5', ',': '-P5', 'n': '+8v', '.': '-8v',
+}
+
 const envelope = { "attack": 0.01, "decay": 0.01, "sustain": 0.75, "release": 3 }
 
 
 
 // Model *************************************************************************
-let currentRootNote = 'C3';
+const state = {
+  handed: 'left',
+  currentRootNote: 'C3',
+  portamento: 0,
+}
+
 const setCurrentRootNote = function(thing) {
-  let noteIndex = fullRange.indexOf(currentRootNote)
+  let noteIndex = fullRange.indexOf(state.currentRootNote)
   noteIndex = noteIndex + intervalConversions[thing]
   if (noteIndex > 0 && noteIndex <= fullRange.length-1) {
-    currentRootNote = fullRange[noteIndex]
+    state.currentRootNote = fullRange[noteIndex]
   }
 }
 
@@ -42,24 +63,50 @@ const synth = new Tone.Synth(
 ).toMaster()
 
 const playSynth = function() {
-    synth.triggerAttackRelease(currentRootNote, '8n')
+    synth.triggerAttackRelease(state.currentRootNote, '8n')
 
 }
 
 
-// View ***************************************************************************
-const currentRootNoteView = $('#current-root-note')
+// View ****************************************************************************
 
-const setCurrentRootNoteView = function(noteDisplay) {
-  currentRootNoteView.html(noteDisplay)
+const leftHandView = $('#left-hand')
+const rightHandView = $('#right-hand')
+
+const setLeftHandNoteView = function(noteDisplay) {
+  leftHandView.html(noteDisplay)
+}
+
+const setRightHandNoteView = function(noteDisplay) {
+  rightHandView.html(noteDisplay)
 }
 
 
 // Boss ***************************************************************************
-const interpretKeypress = function(stuff) {
-  setCurrentRootNote(stuff)
-  setCurrentRootNoteView(currentRootNote)
-  playSynth(currentRootNote)
+const interpretKeypress = function(zone, pressed) {
+  switch(zone) {
+    case 'general':
+
+      break
+    case 'left':
+      if (state.handed==='left') {
+        setCurrentRootNote(LH_pitch_keys[pressed])
+        setLeftHandNoteView(state.currentRootNote)
+        playSynth(state.currentRootNote)
+      } else {
+        console.log('pressed', pressed)
+      }
+      break
+    case 'right':
+      if (state.handed==='right') {
+        setCurrentRootNote(RH_pitch_keys[pressed])
+        setRightHandNoteView(state.currentRootNote)
+        playSynth(state.currentRootNote)
+      } else {
+        console.log('pressed', pressed)
+      }
+      break
+  }
 }
 
 
--- js/pitch_shifts.js
+++ js/pitch_shifts.js
@@ -2,12 +2,22 @@
 
 const general_pitch_shifts = [
   {
-    "keys": "space",
+    "keys": "control z",
     "on_keydown": function () {
-      // console.log('space')
-      interpretKeypress('cur')
+      console.log('control z')
+      interpretKeypress('general', 'ctrl-z')
+    },
+    "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": true,
+    "is_exclusive": true,
+  },
+  {
+    "keys": "control /",
+    "on_keydown": function () {
+      console.log('control /')
+      interpretKeypress('general', 'ctrl-z')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   }
 ]
 
@@ -15,239 +25,290 @@ const general_pitch_shifts = [
 // LH PITCH SHIFTS *************************************************************************************************
 const LH_pitch_shift = [
   {
+    "keys": "space",
+    "on_keydown": function () {
+      // console.log('space')
+      interpretKeypress('left', 'space')
+    },
+    "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
+  },
+  {
     "keys": "f",
     "on_keydown": function () {
       // console.log('f')
-      interpretKeypress('+m1')
+      interpretKeypress('left', 'f')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "d",
     "on_keydown": function () {
       // console.log('d')
-      interpretKeypress('-m1')
+      interpretKeypress('left', 'd')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "g",
     "on_keydown": function () {
       // console.log('g')
-      interpretKeypress('+M2')
+      interpretKeypress('left', 'g')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "s",
     "on_keydown": function () {
       // console.log('s')
-      interpretKeypress('-M2')
+      interpretKeypress('left', 's')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "r",
     "on_keydown": function () {
-      interpretKeypress('+m3')
+      interpretKeypress('left', 'r')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "e",
     "on_keydown": function () {
-      interpretKeypress('-m3')
+      interpretKeypress('left', 'e')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "t",
     "on_keydown": function () {
-      interpretKeypress('+M3')
+      interpretKeypress('left', 't')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "w",
     "on_keydown": function () {
-      interpretKeypress('-M3')
+      interpretKeypress('left', 'w')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "4",
     "on_keydown": function () {
-      interpretKeypress('+P4')
+      interpretKeypress('left', '4')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "3",
     "on_keydown": function () {
-      interpretKeypress('-P4')
+      interpretKeypress('left', '3')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "5",
     "on_keydown": function () {
-      interpretKeypress('+tt')
+      interpretKeypress('left', '5')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "2",
     "on_keydown": function () {
-      interpretKeypress('-tt')
+      interpretKeypress('left', '2')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "c",
     "on_keydown": function () {
-      interpretKeypress('+P5')
+      interpretKeypress('left', 'c')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "x",
     "on_keydown": function () {
-      interpretKeypress('-P5')
+      interpretKeypress('left', 'x')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "v",
     "on_keydown": function () {
-      interpretKeypress('+8v')
+      interpretKeypress('left', 'v')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "z",
     "on_keydown": function () {
-      interpretKeypress('-8v')
+      interpretKeypress('left', 'z')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   }
 ]
 
 // RH PITCH SHIFTS *************************************************************************************************
 const RH_pitch_shift = [
+
+  {
+    "keys": "space",
+    "on_keydown": function () {
+      // console.log('space')
+      interpretKeypress('right', 'space')
+    },
+    "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
+  },
   {
     "keys": "j",
     "on_keydown": function () {
       // console.log('f')
-      interpretKeypress('+m1')
+      interpretKeypress('right', 'j')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "k",
     "on_keydown": function () {
       // console.log('d')
-      interpretKeypress('-m1')
+      interpretKeypress('right', 'k')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "h",
     "on_keydown": function () {
       // console.log('g')
-      interpretKeypress('+M2')
+      interpretKeypress('right', 'h')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "l",
     "on_keydown": function () {
       // console.log('s')
-      interpretKeypress('-M2')
+      interpretKeypress('right', '1')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "i",
     "on_keydown": function () {
-      interpretKeypress('+m3')
+      interpretKeypress('right', 'i')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "o",
     "on_keydown": function () {
-      interpretKeypress('-m3')
+      interpretKeypress('right', 'o')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "u",
     "on_keydown": function () {
-      interpretKeypress('+M3')
+      interpretKeypress('right', 'u')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "p",
     "on_keydown": function () {
-      interpretKeypress('-M3')
+      interpretKeypress('right', 'p')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "8",
     "on_keydown": function () {
-      interpretKeypress('+P4')
+      interpretKeypress('right', '8')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "9",
     "on_keydown": function () {
-      interpretKeypress('-P4')
+      interpretKeypress('right', '9')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "7",
     "on_keydown": function () {
-      interpretKeypress('+tt')
+      interpretKeypress('right', '7')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "0",
     "on_keydown": function () {
-      interpretKeypress('-tt')
+      interpretKeypress('right', '0')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "m",
     "on_keydown": function () {
-      interpretKeypress('+P5')
+      interpretKeypress('right', 'm')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": ",",
     "on_keydown": function () {
-      interpretKeypress('-P5')
+      interpretKeypress('right', ',')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": "n",
     "on_keydown": function () {
-      interpretKeypress('+8v')
+      interpretKeypress('right', 'n')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   },
   {
     "keys": ".",
     "on_keydown": function () {
-      interpretKeypress('-8v')
+      interpretKeypress('right', '.')
     },
     "this": this, "prevent_default": true, "prevent_repeat": true, "is_unordered": true, "is_sequence": false,
+    "is_exclusive": true,
   }
 ]
